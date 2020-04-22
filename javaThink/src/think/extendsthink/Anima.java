eec487797fe6c57f832e0f09a812b30d75c48665
lidaxia2020 <843180879@qq.com>
2020-04-22 08:13:15 +0800

    add mysql和操作符内容

---
+++ javaThink/src/think/extendsthink/Anima.java
@@ -0,0 +1,12 @@
+package think.extendsthink;
+
+/**
+ * @Auther lidaxia
+ * @Date 2020-04-16 23:25
+ */
+public class Anima
+{
+    void speak(){
+        System.out.println("anima speak");
+    }
+}
---
+++ javaThink/src/think/extendsthink/Dog.java
@@ -0,0 +1,34 @@
+package think.extendsthink;
+
+import java.util.Arrays;
+
+/**
+ * @Auther lidaxia
+ * @Date 2020-04-16 23:26
+ */
+public class Dog extends Anima
+{
+    @Override
+    void speak() {
+        super.speak();
+        System.out.println("dog speak");
+    }
+
+    public void run(){
+        System.out.println("dog run");
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 子类可以继承父类的功能，但父类只能感知自己拥有的方法
+         */
+        Anima anima = new Dog();
+        anima.speak();
+        String a ="a";
+        a.equals("a");
+        int i = 1000;
+        System.out.println(Integer.toBinaryString(i));
+    }
+}
+
+
